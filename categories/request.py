8acfa11fd943ffc314c7df64575672ae51d887fd
Alicia AyersLott <amayers92@gmail.com>
2021-10-25 13:30:33 -0500

    cleaning up categories

--- categories/request.py
+++ categories/request.py
@@ -20,9 +20,7 @@ def get_all_categories():
         dataset = db_cursor.fetchall()
 
         for row in dataset:
-
             category = Category(row['id'], row['label'])
-            
             categories.append(category.__dict__)
 
     return json.dumps(categories)
