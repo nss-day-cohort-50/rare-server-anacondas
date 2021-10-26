519a14d558ed82d3f37916a194fe1e6b85b8c7be
Blake Rhoden <zbrhoden@gmail.com>
2021-10-26 16:02:28 -0500

    Post to postman

--- models/post.py
+++ models/post.py
@@ -1,10 +1,9 @@
 class Post():
 
-    def __init__(self, id, user_id, category_id, title, publication_date, content, approved):
+    def __init__(self, id, user_id, category_id, title, publication_date, content):
         self.id = id
         self.user_id = user_id
         self.category_id = category_id,
         self.title = title
         self.publication_date = publication_date
         self.content = content
-        self.approved = approved 
