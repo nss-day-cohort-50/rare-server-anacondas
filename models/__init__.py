10ff26d2025326483eb1d7d238eb70d9a29af295
Ben Gregory <JBGregoryPro@protonmail.com>
2021-10-26 13:08:22 -0500

    get-all working

--- models/__init__.py
+++ models/__init__.py
@@ -1,14 +1,7 @@
-<<<<<<< HEAD
 from .category import Category
 from .comment import Comment
 from .post import Post
 from .tag import Tag
 from .user import User
-=======
-from category import Category
-from comment import Comment
-from post import Post
-from tag import Tag
-from user import User
-from subscription import Subscription
->>>>>>> main
+from .subscription import Subscription
+from. reaction import Reaction
--- request_handler.py
+++ request_handler.py
@@ -3,7 +3,6 @@ from categories import get_all_categories, get_single_category, create_category,
 from posts import get_all_posts, get_single_post, create_post, delete_post, update_post
 from tags import get_all_tags, get_single_tag, create_tag, delete_tag, update_tag
 from comments import get_all_comments, get_single_comment, create_comment, delete_comment, update_comment
-from posts import get_all_posts, get_single_post, create_post, delete_post, update_post
 from users import get_all_users, get_single_user, create_user, delete_user, update_user
 from reactions import get_all_reactions, get_single_reaction, create_reaction, delete_reaction, update_reaction
 import json
